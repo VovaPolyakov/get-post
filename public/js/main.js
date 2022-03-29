38a436a94409269dc5764d68cd955c92ff5f923e
Vladimir <1337vovalenovo@gmail.com>
2022-03-29 22:03:46 +0300

    adding travel sorting

--- public/js/main.js
+++ public/js/main.js
@@ -14,33 +14,71 @@ const tickets = [
 ];
 
 time.addEventListener('click',function(event){
-    let array =[];
-    for(let i = 0;i<tickets.length;i++){
-        array.push(tickets[i].time)
+    if(event.target.classList.contains('no_sort')){
+        sortingIncrease();
+        time.classList.remove('no_sort')
+        time.classList.add('increase')
+    }else if(event.target.classList.contains('increase')){
+        sortingDescending()
+        time.classList.remove('increase')
+        time.classList.add('descending')
+    }else if(event.target.classList.contains('descending')){
+        sortingIncrease();
+        time.classList.remove('descending')
+        time.classList.add('increase')
     }
-    sorting(array)
 
-    function sorting(tickets){
-        for(let n =0;n<array.length;n++){
-            for(let i = 0;i<tickets.length;i++){
-                if(tickets[i] >tickets[i+1]){
-                    const array = tickets[i]
-                    tickets[i] = tickets[i+1]
-                    tickets[i+1] = array
+    console.log(event.target)
+    function sortingIncrease(){
 
+        let array =[];
+        for(let i = 0;i<tickets.length;i++){
+            array.push(tickets[i].time)
+        }
+        sorting(array)
+
+    
+        function sorting(tickets){
+            for(let n =0;n<array.length;n++){
+                for(let i = 0;i<tickets.length;i++){
+                    if(tickets[i] >tickets[i+1]){
+                        const array = tickets[i]
+                        tickets[i] = tickets[i+1]
+                        tickets[i+1] = array
+                    }
                 }
             }
         }
-    }
-    for(let i = 0;i< array.length;i++){
-        let adults = tickets.filter(ticket =>{
-            if(ticket.time == array[i]){
-                return true
+        for(let i = 0;i< array.length;i++){
+            let adults = tickets.filter(ticket =>{
+                if(ticket.time == array[i]){
+                    return true
+                }
+            })
+            for(let n = 0;n< adults.length;n++){
+                tableTime[i].innerText = adults[n].time;
+                tableCountry[i].innerText = adults[n].country;   
             }
-        })
-        for(let n = 0;n< adults.length;n++){
-            tableTime[i].innerText = adults[n].time;
-            tableCountry[i].innerText = adults[n].country;   
+        }
+    }
+    function sortingDescending(){
+        let array =[];
+        for(let i = 0;i<tickets.length;i++){
+            array.push(tickets[i].time)
+        }
+    
+        array.sort((a, b) => b - a);
+
+        for(let i = 0;i< array.length;i++){
+            let adults = tickets.filter(ticket =>{
+                if(ticket.time == array[i]){
+                    return true
+                }
+            })
+            for(let n = 0;n< adults.length;n++){
+                tableTime[i].innerText = adults[n].time;
+                tableCountry[i].innerText = adults[n].country;   
+            }
         }
     }
 })
